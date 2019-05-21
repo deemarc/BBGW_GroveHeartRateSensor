b69a164c85b618e9c8af40fe60a7853f62ae416f
deemarc <marteleus@gmail.com>
2019-05-22 02:11:31 +0700

    wrong i2c lib, fixed

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource
-import smbus
+# import smbus
+import Adafruit_GPIO.I2C as I2C
 app = Flask(__name__)
 api = Api(app)
 
 class heartRate(Resource):
     def get(self):
-        bus = smbus.SMBus(2)
-        curBPM = bus.read_byte_data(0xA0,0x50)
+        i2c = I2C.Device(0x50,2)
+        curBPM = i2c.readS8(0xA0)
         retJson = {"curBPM":curBPM}
-        return retJson, 200
+        return retJson
 
 api.add_resource(heartRate, "/")
 # api.add_resource(addTwoNumber, "/addTwoNumber")
@@ -18,4 +19,4 @@ api.add_resource(heartRate, "/")
 # api.add_resource(divideTwoNum, "/divideTwoNum")
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
